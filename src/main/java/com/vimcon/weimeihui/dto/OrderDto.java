354252f9794acbeb2e4f461cb1b5fd78717b7e77
Bo Hou <hou_b@worksap.co.jp>
2016-01-12 19:03:03 +0900

    add dto

--- src/main/java/com/vimcon/weimeihui/dto/OrderDto.java
+++ src/main/java/com/vimcon/weimeihui/dto/OrderDto.java
@@ -1,5 +1,23 @@
 package com.vimcon.weimeihui.dto;
 
-public class OrderDto {
+import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.joda.time.DateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDto {
+	private long orderId;
+	private DateTime orderDate;
+	private DateTime returnDate;
+	private String customerId;
+	private String customerName;
+	private List<OrderItem> items;
+	private boolean isDone;
+	
 }
---
+++ src/main/java/com/vimcon/weimeihui/dto/OrderItem.java
@@ -0,0 +1,24 @@
+package com.vimcon.weimeihui.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.joda.money.Money;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+	private String itemName;
+	private Money costJPY;
+	private int quantity;
+	private Money expressFeeJPY;
+	private Money giftCostJPY;
+	
+	private Money costRMB;
+	private Money expressFeeRMB;
+	private Money giftCostRMB;
+	private Money costSumJPY;
+	private Money costSumRMB;
+}
